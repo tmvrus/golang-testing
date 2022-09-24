7531988dc2a1b79feffee1e817bcccce92ac9eb2
Mikhail Tsimbalov <tmvrus83@gmail.com>
2022-09-24 16:11:20 +0300

    move mock back

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -8,6 +8,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 
 	"github.com/tmvrus/golang-testing/handler"
@@ -36,14 +37,14 @@ func Test_Handler(t *testing.T) {
 			in:   []byte(`{"jsonrpc":"2.0","method":"registerPayout","id":1,"Params":{"user_id":"632d90378b2cb2b83351f130","payout":10}}`),
 			want: `{"error":{"code":-32002, "data":"TOKEN", "message":"user not authorized"}, "id":1, "jsonrpc":"2.0", "result":null}`,
 			setupMock: func(auth *mock.MockAuthorizator, stor *mock.MockPayout) {
-				//m := map[string]string{
-				//	"token":   "TOKEN",
-				//	"user_id": "632d90378b2cb2b83351f130",
-				//}
-				//auth.
-				//	EXPECT().
-				//	Authorized(gomock.Any(), matcher{m:m}).
-				//	Return(false, nil)
+				m := map[string]string{
+					"token":   "TOKEN",
+					"user_id": "632d90378b2cb2b83351f130",
+				}
+				auth.
+					EXPECT().
+					Authorized(gomock.Any(), m).
+					Return(false, nil)
 			},
 		},
 	}
@@ -52,15 +53,20 @@ func Test_Handler(t *testing.T) {
 		test := tests[i]
 		t.Run(test.name, func(t *testing.T) {
 			t.Parallel()
+			ctr := gomock.NewController(t)
+			t.Cleanup(ctr.Finish)
+			authMock := mock.NewMockAuthorizator(ctr)
 
+			if test.setupMock != nil {
+				test.setupMock(authMock, nil)
+			}
 			req := httptest.NewRequest(http.MethodPost, "/", bytes.NewReader(test.in))
 			req.Header.Add("Content-Type", "application/json")
 			req.Header.Add("API-Token", "TOKEN")
 			w := httptest.NewRecorder()
 
-			auth := authMock{}
 			smock := storMock(func() {})
-			handler.NewHandler(auth, smock).ServeHTTP(w, req)
+			handler.NewHandler(authMock, smock).ServeHTTP(w, req)
 
 			response, err := io.ReadAll(w.Result().Body)
 			require.NoError(t, err)
@@ -77,14 +83,4 @@ func (storMock) Register(ctx context.Context, userID string, reqID int64, payout
 }
 func (storMock) Count(ctx context.Context, userID string) (int64, error) {
 	return 0, nil
-}
-
-type authMock struct {
-}
-
-func (authMock) Authorized(ctx context.Context, m map[string]string) (bool, error) {
-	if m["token"] == "TOKEN" {
-		return false, nil
-	}
-	return false, nil
 }
